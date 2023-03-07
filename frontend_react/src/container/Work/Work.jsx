fee925896032a6c54da068d81c33c7114540b1d3
Dimitri <dsz@Dimitris-Air.hsd1.fl.comcast.net>
2023-03-07 11:54:13 -0500

    directory title

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
 
+import { urlFor, client } from '../../client';
 import './Work.scss';
 
 const Work = () => {
   return (
-    <div>Work</div>
+    <>
+    <br></br>
+    <br></br>
+    <br></br>
+      <h2 className="head-text">My Creative <span>Portfolio</span></h2>
+    </>
   )
 }
 
